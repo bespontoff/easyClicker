dd8e8a91a636178413a32fb1aa9bb8c684ce5bc5
bespontoff <bespontoff@gmail.com>
2021-10-18 09:22:28 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='easyClicker',
+    version='1.2.1',
+    packages=[''],
+    url='https://github.com/bespontoff/easyClicker',
+    license=' MIT License ',
+    author='bespontoff',
+    author_email='bespontoff@gmail.com',
+    description='makes clicks at chocked place'
+)
